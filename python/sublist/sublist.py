2765a480faa5194914562bfa82bebe6079815814
Miles K. Rains <mileskrains@gmail.com>
2017-11-21 10:30:46 -0800

    minor cleanup

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -1,13 +1,11 @@
 SUBLIST, SUPERLIST, EQUAL, UNEQUAL = 'sublist superlist equal unequal'.split()
 
+
 def check_lists(first_list, second_list):
     len_first = len(first_list)
     len_second = len(second_list)
-    if len_first == len_second:
-        if first_list == second_list:
-            return EQUAL
-        else:
-            return UNEQUAL
+    if first_list == second_list:
+         return EQUAL
     elif len_first > len_second and is_sublist(first_list, second_list):
         return SUPERLIST
     elif len_first < len_second and is_sublist(second_list, first_list):
