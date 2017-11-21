946c0cc4996784f15201cc05a5b725a5d9dc74d1
Miles K. Rains <mileskrains@gmail.com>
2017-11-21 10:35:39 -0800

    naming change

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -6,15 +6,15 @@ def check_lists(first_list, second_list):
     len_second = len(second_list)
     if first_list == second_list:
          return EQUAL
-    elif len_first > len_second and is_sublist(first_list, second_list):
+    elif len_first > len_second and is_sublist_of(first_list, second_list):
         return SUPERLIST
-    elif len_first < len_second and is_sublist(second_list, first_list):
+    elif len_first < len_second and is_sublist_of(second_list, first_list):
         return SUBLIST
     else:
         return UNEQUAL
 
 
-def is_sublist(first_list, second_list):
+def is_sublist_of(first_list, second_list):
     second_len = len(second_list)
     for sp in range(len(first_list) - second_len + 1):
         if first_list[sp:sp + second_len] == second_list:
