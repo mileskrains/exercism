d4015b3d039351b280ac0e0e67e6a29b0f4d73c0
Miles K. Rains <mileskrains@gmail.com>
2017-11-27 11:26:12 -0800

    finished saddle points

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,11 @@
 def saddle_points(matrix):
-    pass
+    if len(set(map(len, matrix))) > 1:
+        raise ValueError('matrix is not square')
+    columns = list(zip(*matrix))
+    sad_pts = set()
+    for r, row in enumerate(matrix):
+        for c, col in enumerate(columns):
+            e = row[c]
+            if e == max(row) and e == min(col):
+                sad_pts.add((r, c))
+    return sad_pts
