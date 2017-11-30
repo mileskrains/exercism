8f320517b35b6ac05ccad487dd253c8b03654019
Miles K. Rains <mileskrains@gmail.com>
2017-11-30 09:52:19 -0800

    minor improvement

--- python/house/house.py
+++ python/house/house.py
@@ -1,21 +1,21 @@
 def verse(vn):
-    lines = (('the house that Jack built.', ''),
-             ('the malt', 'lay in'),
-             ('the rat', 'ate'),
-             ('the cat', 'killed'),
-             ('the dog', 'worried'),
-             ('the cow with the crumpled horn', 'tossed'),
-             ('the maiden all forlorn', 'milked'),
-             ('the man all tattered and torn', 'kissed'),
-             ('the priest all shaven and shorn', 'married'),
-             ('the rooster that crowed in the morn', 'woke'),
-             ('the farmer sowing his corn', 'kept'),
-             ('the horse and the hound and the horn', 'belonged to'))
-    verse = 'This is '
+    lines = (('house that Jack built.', ''),
+             ('malt', 'lay in'),
+             ('rat', 'ate'),
+             ('cat', 'killed'),
+             ('dog', 'worried'),
+             ('cow with the crumpled horn', 'tossed'),
+             ('maiden all forlorn', 'milked'),
+             ('man all tattered and torn', 'kissed'),
+             ('priest all shaven and shorn', 'married'),
+             ('rooster that crowed in the morn', 'woke'),
+             ('farmer sowing his corn', 'kept'),
+             ('horse and the hound and the horn', 'belonged to'))
+    verse = 'This is'
     for n in range(vn, -1 , -1):
         noun, verb = lines[n]
-        verse += noun
-        verse += f'\nthat {verb} ' if verb else '\n'
+        verse += f' the {noun}'
+        verse += f'\nthat {verb}' if verb else '\n'
     return verse.strip()
 
 
