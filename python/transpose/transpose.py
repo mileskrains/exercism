b6e14a7db3eecbb33454d21f1e2cdbdd4752e41e
Miles K. Rains <mileskrains@gmail.com>
2017-12-01 10:52:33 -0800

    minor edits

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,10 +1,10 @@
 from itertools import zip_longest
 
+
 def transpose(input_lines):
     if not input_lines:
         return ''
-    inp = input_lines.split('\n')
-    inp = [line.replace(' ', '~') for line in inp]
+    inp = [line.replace(' ', '~') for line in input_lines.split('\n')]
     transpose = list(map(lambda z: ''.join(z),
                          zip_longest(*inp, fillvalue=' ')))
     transpose = [line.rstrip().replace('~', ' ') for line in transpose]
