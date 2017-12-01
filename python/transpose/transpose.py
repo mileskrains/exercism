515bf1b95f7878c6ae139fb6a1d70c3d211254cf
Miles K. Rains <mileskrains@gmail.com>
2017-12-01 10:48:01 -0800

    reverted transpose tests to orginal, and fixed transpose.py

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,14 +1,11 @@
 from itertools import zip_longest
 
 def transpose(input_lines):
-    if input_lines == '':
+    if not input_lines:
         return ''
-    if type(input_lines) == str:
-        input_lines = [input_lines]
-    inp = [line.replace(' ', '~') for line in input_lines]
+    inp = input_lines.split('\n')
+    inp = [line.replace(' ', '~') for line in inp]
     transpose = list(map(lambda z: ''.join(z),
                          zip_longest(*inp, fillvalue=' ')))
     transpose = [line.rstrip().replace('~', ' ') for line in transpose]
-    if len(transpose) == 1:
-        transpose = transpose[0]
-    return transpose
+    return '\n'.join(transpose)
