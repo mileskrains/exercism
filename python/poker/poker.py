13299aaae3368a38b46a6c52f084c9119eadc0fa
Miles K. Rains <mileskrains@gmail.com>
2017-12-03 11:30:16 -0800

    more poker tweaks

--- python/poker/poker.py
+++ python/poker/poker.py
@@ -9,8 +9,7 @@ def rank_and_suit_analysis(hand):
     ranks_counter = Counter([rank_val_dict[card[:-1]] for card in hand])
     ranks_mc = sorted(ranks_counter.most_common(),
                       key=itemgetter(1, 0), reverse=True)
-    rank_counts = [c for r, c in ranks_mc]
-    ranks_for_counts = [r for r, c in ranks_mc]
+    ranks_for_counts, rank_counts = zip(*ranks_mc)
     return suit_counts, rank_counts, ranks_for_counts
 
 
@@ -22,19 +21,19 @@ def score(hand):
     suit_counts, rank_counts, ranks_for_counts = rank_and_suit_analysis(hand)
     if suit_counts == [5] and is_sequential(ranks_for_counts):
         hand_score = 9  # straight flush
-    elif rank_counts == [4, 1]:
+    elif rank_counts == (4, 1):
         hand_score = 8  # four of a kind
-    elif rank_counts == [3, 2]:
+    elif rank_counts == (3, 2):
         hand_score = 7  # full house
     elif suit_counts == [5]:
         hand_score = 6  # flush
     elif is_sequential(ranks_for_counts):
         hand_score = 5  # straight
-    elif rank_counts == [3, 1, 1]:
+    elif rank_counts == (3, 1, 1):
         hand_score = 4  # three of a kind
-    elif rank_counts == [2, 2, 1]:
+    elif rank_counts == (2, 2, 1):
         hand_score = 3  # two pair
-    elif rank_counts == [2, 1, 1, 1]:
+    elif rank_counts == (2, 1, 1, 1):
         hand_score = 2  # pair
     else:
         hand_score = 1  # high card
