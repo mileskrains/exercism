839f35d3e29d91b60a2355ac2fd9f07174bceb5a
Miles K. Rains <mileskrains@gmail.com>
2017-12-05 13:04:12 -0800

    abandoning zebra approach 1, will start approach 2

--- python/zebra-puzzle/zebra_puzzle.py
+++ python/zebra-puzzle/zebra_puzzle.py
@@ -24,6 +24,11 @@ class Condition():
         otherspec = [v for k, v in other.__dict__.items() if v]
         return set(selfspec).issubset(set(otherspec))
 
+    def is_complement(self, other):
+        selfspec = [k for k, v in self.__dict__.items() if v]
+        otherspec = [k for k, v in other.__dict__.items() if v]
+        return len(set(selfspec).symmetric_difference(set(otherspec))) == 6
+
     def can_combine(self, other):
         ds = self.__dict__
         do = other.__dict__
@@ -34,6 +39,7 @@ class Condition():
         return True
 
     def combine(self, other):
+        # other adds to self
         ds = self.__dict__
         do = other.__dict__
         combo = {**ds, **{k: v for k, v in do.items() if v}}
@@ -93,19 +99,22 @@ condition_lists = [c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15]
 cl_is_reversible = [c11, c12, c15]
 
 
-def cl_combine_if_common(cl_mrg, cl_targ):
+def cl_combine_if_common_or_complement(cl_mrg, cl_targ):
     alignments = range(len(cl_targ) - len(cl_mrg) + 1)
     for a in alignments:
         matches = False
         combines = True
+        complements = True
 
         for p, c in enumerate(cl_mrg):
             if cl_targ[a + p].has_match(c):
                 matches = True
             if not cl_targ[a + p].can_combine(c):
                 combines = False
+            if not cl_targ[a + p].is_complement(c):
+                complements = False
 
-        if matches and combines:
+        if (matches and combines) or complements:
             for p, c in enumerate(cl_mrg):
                 cl_targ[a + p] = cl_targ[a + p].combine(c)
             return True
@@ -137,7 +146,7 @@ def cl_combine_if_fits_one_spot(cl_mrg, cl_targ):
             cl_targ[a_comb + p] = cl_targ[a_comb + p].combine(c)
 
 
-def cl_in_cl(cl_mrg, cl_targ):
+def _cl_in_cl(cl_mrg, cl_targ):
     alignments = range(len(cl_targ) - len(cl_mrg) + 1)
     for a in alignments:
         is_subset = True
@@ -148,6 +157,62 @@ def cl_in_cl(cl_mrg, cl_targ):
         if is_subset:
             return True
     return False
+
+
+def list_conds_by_field():
+    conds_with_field = []
+    for f in houses[0].__dict__.keys():
+        f_conds = [f]
+        for cl in condition_lists:
+            for c in cl:
+                if getattr(c, f):
+                    if cl not in f_conds:
+                        f_conds.append(cl)
+        conds_with_field.append(f_conds)
+    return conds_with_field
+
+
+def _list_combining_cls(hi, cl_list):
+    combining_list = []  # keep track here, using only unreversed cls
+    for cl in cl_list:
+        if len(cl) == 1:
+            if houses[hi].can_combine(cl[0]):
+                combining_list.append(cl)
+        else:  # any of four patterns which are valid
+            if hi - 1 >= 0:
+                if houses[hi - 1].can_combine(cl[0]) and houses[hi].can_combine(cl[1]):
+                    combining_list.append(cl)
+                if (cl in cl_is_reversible and
+                        houses[hi - 1].can_combine(cl[1]) and
+                        houses[hi].can_combine(cl[0])):
+                    if cl not in combining_list:
+                        combining_list.append(cl)
+            if hi + 1 <= 4:
+                if houses[hi].can_combine(cl[0]) and houses[hi + 1].can_combine(cl[1]):
+                    if cl not in combining_list:
+                        combining_list.append(cl)
+                if (cl in cl_is_reversible and
+                        houses[hi].can_combine(cl[1]) and
+                        houses[hi + 1].can_combine(cl[0])):
+                    if cl not in combining_list:
+                        combining_list.append(cl)
+    return combining_list
+
+
+def cl_combine_if_one_fits_spot():
+    conds_with_field = list_conds_by_field()
+    for fcl in conds_with_field:
+        if len(fcl) > 1:  # has conds
+            f = fcl[0]
+            if f not in ('pet', 'beverage'):  # as these are not fully specified
+                for hi, h in enumerate(houses):
+                    if not getattr(h, f):
+                        combining_cls = _list_combining_cls(hi, fcl[1:])
+                        if len(combining_cls) == 1:
+                            cl_mrg = combining_cls[0]
+                            for p, c in enumerate(cl_mrg):
+                                houses[hi + p] = houses[hi + p].combine(c)
+                        return
 
 
 def view_condition_list(condlist):
@@ -162,21 +227,23 @@ for cl in condition_lists:
     view_condition_list(cl)
     print()
 
-# do come merging ...
-for _ in range(100):
+# do some merging ...
+for _ in range(33):
     combined = []
     for cl in condition_lists:
         for ocl in condition_lists:
             if cl != ocl and len(cl) <= len(ocl):
-                if cl_combine_if_common(cl, ocl):
+                if cl_combine_if_common_or_complement(cl, ocl):
                     combined.append(cl)
-
     condition_lists = [cl for cl in condition_lists if not cl in combined]
 
     for cl in condition_lists:
         cl_combine_if_fits_one_spot(cl, houses)
+        cl_combine_if_common_or_complement(cl, houses)
 
-condition_lists = [cl for cl in condition_lists if not cl_in_cl(cl, houses)]
+    condition_lists = [cl for cl in condition_lists if not _cl_in_cl(cl, houses)]
+
+    cl_combine_if_one_fits_spot()
 
 # see progress / status
 print('\n\n\n')
