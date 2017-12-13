6cb122d8b9ff74bd78707d4fb756abd27d0980f3
Miles K. Rains <mileskrains@gmail.com>
2017-12-13 11:18:56 -0800

    finished binary search

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,13 @@
 def binary_search(list_of_numbers, number):
-    pass
+    start_index = 0
+    end_index = len(list_of_numbers) - 1
+    while end_index >= start_index:
+        middle_index = (start_index + end_index)//2
+        middle_number = list_of_numbers[middle_index]
+        if middle_number == number:
+            return middle_index
+        elif middle_number > number:
+            end_index = middle_index - 1
+        elif middle_number < number:
+            start_index = middle_index + 1
+    raise ValueError
