554a2e5f7046568aa2fd029f0b78bb1a360a6c9f
Miles K. Rains <mileskrains@gmail.com>
2017-12-31 14:10:12 -0800

    finished diamond

---
+++ python/diamond/diamond.py
@@ -0,0 +1,13 @@
+from string import ascii_uppercase
+
+def make_diamond(letter):
+    letters = ascii_uppercase[:ascii_uppercase.index(letter)+1]
+    size = 2*len(letters)-1
+    blank = [list(' ' * size) for _ in range(size)]
+    for ci, ch in enumerate(letters):
+        for ri in (ci, size-ci-1):
+            row = blank[ri]
+            row[len(letters)-1-ci] = ch
+            row[-(len(letters)-ci)] = ch
+    return '\n'.join(list(map(lambda r: ''.join(r), blank))) + '\n'
+
---
+++ python/reverse-string/reverse_string.py
@@ -0,0 +1,2 @@
+def reverse(input=''):
+    return ''.join(c for c in input[::-1])
