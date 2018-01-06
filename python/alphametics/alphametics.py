bab274ccd9bf58cbe2f87a433cacc09592bf39ad
Miles K. Rains <mileskrains@gmail.com>
2018-01-05 21:24:39 -0800

    slight shortening of alphametics.py

--- python/alphametics/alphametics.py
+++ python/alphametics/alphametics.py
@@ -2,20 +2,14 @@ from itertools import permutations
 
 
 def solve(puzzle):
-    lhs, rhs = puzzle.split(' == ')
-    lhs = lhs.split(' + ')
-    letters = set(list(rhs))
-    nonzero = set(rhs[0])
-    for term in lhs:
-        letters.update(list(term))
-        nonzero.add(term[0])
-    conv_keys = list(letters-nonzero) + list(nonzero)
+    words = [w for w in puzzle.split() if w.isalpha()]
+    nonzero = set([w[0] for w in words])
+    letters = list(set(''.join(words))-nonzero) + list(nonzero)
     perms = permutations('0123456789', len(letters))
     for perm in perms:
-        conv_dict = dict(zip(conv_keys, perm))
+        conv_dict = dict(zip(letters, perm))
         if '0' in perm[-len(nonzero):]:
             continue
         if eval(''.join([conv_dict[c] if c.isalpha() else c for c in puzzle])):
             return {k: int(v) for k, v in conv_dict.items()}
     return {}
-
