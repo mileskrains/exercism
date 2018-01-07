7df3fbb544e7ada0c455a48528b2e9e3d1682dc0
Miles K. Rains <mileskrains@gmail.com>
2018-01-06 22:46:17 -0800

    finished tournament

---
+++ python/scale-generator/scale_generator.py
@@ -0,0 +1,3 @@
+class Scale(object):
+    def __init__(self, tonic, scale_name, pattern=None):
+        pass
--- python/tournament/tournament.py
+++ python/tournament/tournament.py
@@ -1,2 +1,34 @@
+from collections import defaultdict
+
+
 def tally(tournament_results):
-    pass
+    results = 'Team'.ljust(31, ' ') + '| MP |  W |  D |  L |  P\n'
+    if not tournament_results:
+        return results.strip()
+    tally_dict = defaultdict(lambda : defaultdict(int))
+    contests = tournament_results.split('\n')
+    for contest in contests:
+        p1, p2, res = contest.split(';')
+        tally_dict[p1]['MP'] += 1
+        tally_dict[p2]['MP'] += 1
+        if res == 'loss':
+            p1, p2 = p2, p1
+            res = 'win'
+        if res == 'win':
+            tally_dict[p1]['W'] += 1
+            tally_dict[p1]['P'] += 3
+            tally_dict[p2]['L'] += 1
+        elif res == 'draw':
+            tally_dict[p1]['D'] += 1
+            tally_dict[p1]['P'] += 1
+            tally_dict[p2]['D'] += 1
+            tally_dict[p2]['P'] += 1
+    table = []
+    for k, v in tally_dict.items():
+        table.append([v[vk] for vk in 'MP W D L P'.split()] + [k])
+    table.sort(key=lambda t: (-t[0], -t[1], -t[2], -t[3], -t[4], t[5]))
+    for mp, w, d, l, p, t in table:
+        results += ' |  '.join([str(val)
+                                for val in (t.ljust(30, ' '), mp, w, d, l, p)]) + '\n'
+    return results.strip()
+
