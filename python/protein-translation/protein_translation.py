ac2c07bcb16a3aa33ba4915f6328b08f80b6d0ee
Miles K. Rains <mileskrains@gmail.com>
2018-01-07 14:24:36 -0800

    finished protein translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,19 @@
 def proteins(strand):
-    pass
+    cp = ('AUG Methionine UUU,UUC Phenylalanine UUA,UUG Leucine '
+          'UCU,UCC,UCA,UCG Serine UAU,UAC Tyrosine UGU,UGC Cysteine '
+          'UGG Tryptophan UAA,UAG,UGA STOP').split()
+    cp_dict = {}
+    for c, protein in zip(cp[::2], cp[1::2]):
+        for codon in c.split(','):
+            cp_dict[codon] = protein
+    stop = False
+    proteins = []
+    while strand and not stop:
+        codon, strand = strand[:3], strand[3:]
+        protein = cp_dict[codon]
+        if protein == 'STOP':
+            stop = True
+        else:
+            proteins.append(protein)
+    return proteins
+
