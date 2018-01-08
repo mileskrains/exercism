617d2cfd6e0f6373bbc436f70ca9cc94aca8bf17
Miles K. Rains <mileskrains@gmail.com>
2018-01-08 10:19:37 -0800

    finished two-fer, tweaked change

--- python/change/change.py
+++ python/change/change.py
@@ -6,25 +6,23 @@ def find_minimum_coins(total_change, coins):
     elif total_change < min(coins):
         return -1
 
-    coins = [c for c in coins if c <= total_change]
     coins.sort()
     tries = []
-    ans = -1
-    len_ans = total_change // min(coins) + 1
     for coin in coins:
         tries.append([coin])
+    ans = -1
+    len_ans = total_change // min(coins) + 1
 
     while tries:
         change = tries.pop()
         if len(change) + 1 < len_ans:
             for coin in coins:
                 if coin <= change[-1]:
-                    if sum(change) + coin == total_change:
+                    if sum(change) + coin < total_change:
+                        tries.append(change + [coin])
+                    elif sum(change) + coin == total_change:
                         change.append(coin)
                         if len(change) < len_ans:
                             ans = sorted(change)
                             len_ans = len(change)
-                    elif sum(change) + coin < total_change:
-                        tries.append(change + [coin])
     return ans
-
