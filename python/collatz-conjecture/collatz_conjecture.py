cb588949d6151176d661b71501e0e5bba293fff5
Miles K. Rains <mileskrains@gmail.com>
2018-01-08 23:59:53 -0800

    finished collatz

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,12 @@
 def collatz_steps(number):
-    pass
+    if number < 1:
+        return None
+    steps = 0
+    while number > 1:
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = 3 * number + 1
+        steps += 1
+    return steps
+
