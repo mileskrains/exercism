f92925372af5495fdefd91fcf47fa105574bdc7e
Miles K. Rains <mileskrains@gmail.com>
2018-01-08 23:39:57 -0800

    tweaked connect

--- python/connect/connect.py
+++ python/connect/connect.py
@@ -15,37 +15,31 @@ class ConnectGame:
             return self.board_dict[0, 0]
         move_rc_offsets = ((-1, 0), (-1, 1), (0, 1), (1, 0), (1, -1), (0, -1))
         winner = ''
-        vis = []
-        unvis = []
-        for sc in range(self.mci+1):
-            if self.board_dict[0, sc] == 'O':
-                unvis.append((0,sc))
-        while unvis and winner == '':
-            r, c = unvis.pop()
-            vis.append((r, c))
-            for ro, co in move_rc_offsets:
-                tr, tc = r + ro, c + co
-                if self.board_dict[tr, tc] == 'O':
-                    if (tr, tc) not in vis:
-                        unvis.append((tr, tc))
-                    if tr == self.mri:
-                        winner = 'O'
-                        break
-        vis = []
-        unvis = []
-        for sr in range(self.mri+1):
-            if self.board_dict[sr, 0] == 'X':
-                unvis.append((sr, 0))
-        while unvis and winner == '':
-            r, c = unvis.pop()
-            vis.append((r, c))
-            for ro, co in move_rc_offsets:
-                tr, tc = r + ro, c + co
-                if self.board_dict[tr, tc] == 'X':
-                    if (tr, tc) not in vis:
-                        unvis.append((tr, tc))
-                    if tc == self.mci:
-                        winner = 'X'
-                        break
+
+        def check():
+            nonlocal winner
+            vis = []
+            while unvis and winner == '':
+                r, c = unvis.pop()
+                vis.append((r, c))
+                for ro, co in move_rc_offsets:
+                    tr, tc = r + ro, c + co
+                    if self.board_dict[tr, tc] == win_char:
+                        if (tr, tc) not in vis:
+                            unvis.append((tr, tc))
+                        if (tr, tc)[win_index] == win_val:
+                            winner = win_char
+                            break
+
+        win_char, win_index, win_val = 'O', 0, self.mri
+        unvis = [(0, sc) for sc in range(self.mci + 1)
+                 if self.board_dict[0, sc] == win_char]
+        check()
+
+        win_char, win_index, win_val = 'X', 1, self.mci
+        unvis = [(sr, 0) for sr in range(self.mri + 1)
+                 if self.board_dict[sr, 0] == win_char]
+        check()
+
         return winner
 
