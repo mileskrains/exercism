5979a290f46d97117bb6a1bf6d6934b55e2ef1ae
Miles K. Rains <mileskrains@gmail.com>
2018-01-11 14:35:23 -0800

    naming clarifications

--- python/two-bucket/two_bucket.py
+++ python/two-bucket/two_bucket.py
@@ -1,33 +1,30 @@
 def do_op(b1, b2, op):
     c1, s1 = b1
     c2, s2 = b2
-    if op == 'f':
+    if op == 'fill':
         return (s1, s1), (c2, s2)
-    elif op == 'e':
+    elif op == 'empty':
         return (c1, s1), (0, s2)
-    elif op == 't':
+    elif op == 'transfer':
         e2 = s2 - c2
         t = c1 if c1 <= e2 else e2
         return (c1 - t, s1), (c2 + t, s2)
 
 
 def two_bucket(bucket_one_cap, bucket_two_cap, desired_liters, first):
-    c1, c2 = bucket_one_cap, bucket_two_cap
+    b1 = 0, bucket_one_cap
+    b2 = 0, bucket_two_cap
     if first == 'two':
-        c1, c2 = c2, c1
-    b1 = 0, c1
-    b2 = 0, c2
+        b1, b2 = b2, b1
     op_ct = 0
     while not desired_liters in (b1[0], b2[0]):
         if b1[0] == 0:
-            b1, b2 = do_op(b1, b2, 'f')
-            op_ct += 1
+            b1, b2 = do_op(b1, b2, 'fill')
         elif b2[0] == b2[1]:
-            b1, b2 = do_op(b1, b2, 'e')
-            op_ct += 1
+            b1, b2 = do_op(b1, b2, 'empty')
         else:
-            b1, b2 = do_op(b1, b2, 't')
-            op_ct += 1
+            b1, b2 = do_op(b1, b2, 'transfer')
+        op_ct += 1
     if first == 'two':
         b1, b2 = b2, b1
     if b1[0] == desired_liters:
