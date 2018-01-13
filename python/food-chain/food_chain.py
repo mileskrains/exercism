1e8a16803ee83f6792640597ad5af557e33fad2a
Miles K. Rains <mileskrains@gmail.com>
2018-01-12 16:41:49 -0800

    var name improvements

--- python/food-chain/food_chain.py
+++ python/food-chain/food_chain.py
@@ -2,7 +2,7 @@ def chain():
     know = "I know an old lady who swallowed a {}."
     swallowed = "She swallowed the {} to catch the {}."
 
-    why_chain = (
+    anims_remarks = (
         ('fly', "I don't know why she swallowed the fly. Perhaps she'll die."),
         ('spider', 'It wriggled and jiggled and tickled inside her.'),
         ('bird', 'How absurd to swallow a bird!'),
@@ -14,17 +14,17 @@ def chain():
     )
 
     lyrics = []
-    for wi, (animal, remark) in enumerate(why_chain):
+    for wi, (animal, remark) in enumerate(anims_remarks):
         lyrics.append(know.format(animal))
         lyrics.append(remark)
         if 0 < wi < 7:
             for i in range(wi, 0, -1):
-                catcher = why_chain[i][0]
-                caught = why_chain[i - 1][0]
+                catcher = anims_remarks[i][0]
+                caught = anims_remarks[i - 1][0]
                 if caught == 'spider':
                     caught += ' that wriggled and jiggled and tickled inside her'
                 lyrics.append(swallowed.format(catcher, caught))
-            lyrics.append(why_chain[0][1])
+            lyrics.append(anims_remarks[0][1])
         lyrics.append('')
 
     return '\n'.join(lyrics).strip()
