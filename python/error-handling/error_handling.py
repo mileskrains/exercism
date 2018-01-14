ed62e779eeb1f8f961a3e8b0417727aa4ae0e99f
Miles K. Rains <mileskrains@gmail.com>
2018-01-14 15:49:41 -0800

    finished error handling

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -1,14 +1,26 @@
 def handle_error_by_throwing_exception():
-    pass
+    raise Exception('somthing is wrong')
 
 
 def handle_error_by_returning_none(input_data):
-    pass
+    try:
+        return int(input_data)
+    except:
+        return None
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    try:
+        return True, int(input_data)
+    except:
+        return False, None
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-    pass
+    try:
+        filelike_object.do_something()
+    except:
+        raise
+    finally:
+        filelike_object.close()
+
