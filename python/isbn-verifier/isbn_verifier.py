6c9d70d1885dd29d1a29b79d7f87c99d62e311cd
Miles K. Rains <mileskrains@gmail.com>
2018-01-18 15:15:29 -0800

    progress on react (passes 6 of 7 tests), jumping ahead for now

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -6,4 +6,4 @@ def verify(isbn):
         10 in digs[:9] or
         not (isbn[-1].isdigit() or isbn[-1]== 'X')):
         return False
-    return sum([ d * (10-i) for i, d in enumerate(digs)]) % 11 == 0
+    return sum([d * (10 - i) for i, d in enumerate(digs)]) % 11 == 0
--- python/react/react.py
+++ python/react/react.py
@@ -1,17 +1,39 @@
 class Cell(object):
-    def set_value(value):
-        pass
+    def __init__(self):
+        self.watchers = []
+        self.value = None
+
+    def set_value(self, value):
+        if value == self.value:
+            return
+        self.value = value
+        for w in self.watchers:
+            if type(w) == tuple:
+                cc, dependencies, updater_callback = w
+                args = [c.value for c in dependencies]
+                cc.set_value(updater_callback(args))
+            else:
+                w(self, self.value)
 
     def add_watcher(self, watcher_callback):
-        pass
+        if watcher_callback not in self.watchers:
+            self.watchers.append(watcher_callback)
 
     def remove_watcher(self, watcher_callback):
-        pass
+        if watcher_callback in self.watchers:
+            self.watchers.remove(watcher_callback)
 
 
 class Reactor(object):
     def create_input_cell(self, value):
-        pass
+        ic = Cell()
+        ic.set_value(value)
+        return ic
 
     def create_compute_cell(self, dependencies, updater_callback):
-        pass
+        cc = Cell()
+        for dep in dependencies:
+            dep.add_watcher((cc, dependencies, updater_callback))
+        args = [c.value for c in dependencies]
+        cc.value = updater_callback(args)
+        return cc
