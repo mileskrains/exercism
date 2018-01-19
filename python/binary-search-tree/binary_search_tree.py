f11ecba3c6d64500f80b5623880c487b21e81c7f
Miles K. Rains <mileskrains@gmail.com>
2018-01-18 16:21:24 -0800

    finished binary search tree

--- python/binary-search-tree/binary_search_tree.py
+++ python/binary-search-tree/binary_search_tree.py
@@ -1,14 +1,37 @@
 class TreeNode(object):
     def __init__(self, value):
         self.value = value
+        self.left = None
+        self.right = None
 
 
 class BinarySearchTree(object):
     def __init__(self):
-        pass
+        self.root = None
+
+    def list(self, node=None, vals=None):
+        if node == None:
+            node = self.root
+            vals = []
+        if node.left:
+            self.list(node.left, vals)
+        vals.append(node.value)
+        if node.right:
+            self.list(node.right, vals)
+        return vals
 
     def add(self, value):
-        pass
+        if self.root == None:
+            self.root = TreeNode(value)
+        else:
+            parent = self.root
+            while (value <= parent.value and parent.left or
+                   value > parent.value and parent.right):
+                parent = parent.left if value <= parent.value else parent.right
+            if value > parent.value:
+                parent.right = TreeNode(value)
+            else:
+                parent.left = TreeNode(value)
 
     def search(self, value):
-        pass
+        return TreeNode(value) if value in self.list() else None
